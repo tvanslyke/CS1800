1b4d27b567055575d89c81680d7c54af194e226f
Tim Van Slyke <vanslyke.t@husky.neu.edu>
2016-10-15 15:29:57 -0400

    Small adjustments

--- CS1800.py
+++ CS1800.py
@@ -11,7 +11,8 @@ def lcm(a, b):
 
 def inverse(a, b, both = False):
     """ Multiplicative inverse of 'a' in mod 'b'.
-        both = True give"""
+        both = True gives the coefficients 'A' and 'B' of
+        the equation A*x + B*y = gcd(x,y)"""
     x = [1, 0]
     y = [0, 1]
     r = [a, b]
@@ -28,23 +29,21 @@ def inverse(a, b, both = False):
         return x[-1] % b if r[0] == 1 else None    
 
 def primefact(number):
-    """ Prime factorization of 'number'"""
+    """ Prime factorization of 'number'."""
 
-    if isprime(number):
+    if isprime(number) or number == 1:
         return [(number, 1)]
     else:
         factors = []
         counts = []
         for primenum in (prime(n) for n in xrange(1, number)):
-            if number == 1:
-                break
-            elif not number % primenum:
+            if not number % primenum:
                 factors.append(primenum)
                 counts.append(1)
                 number = number // primenum
                 while not (number % primenum):
                     counts[-1] += 1
-                    number = number / primenum
+                    number = number // primenum
         return zip(factors, counts)
 
 def encrypt(message, a, b):
@@ -70,10 +69,22 @@ def decrypt(message, a, b):
     decrypted = [(a * (char + b)) % 26 for char in message]
     return "".join([chr(char+65) for char in decrypted]), decrypted
 
-def cartesian(s1, s2):
-    """ Returns the cartesian product of two sets.  This can get very big."""
-    return set([(el1, el2) for el1 in s1 for el2 in s2])
+def cartesian(*sets):
+    """ Returns the cartesian product of the argument sets.  This can get very big."""
+    def product(s1, s2):
+        return set([(el1, el2) for el1 in s1 for el2 in s2])
+    def merge(s1, s2):
+        return set(tuple(list(el1) + [el2]) for el1 in s1 for el2 in s2)
+    master = product(sets[0], sets[1])
+    for s in sets[2:]:
+        master = merge(master, s)
+    return master
 
+def subsets(s, cardinality):
+    """ Returns a set of subsets with the specified cardinality.
+        The subsets will be selected from input set 's'."""
+    
+    
 def powerset(s):
     """ Returns the powerset of a set.  Subsets will be instances of
         the 'frozenset' type. """
@@ -86,7 +97,13 @@ def powerset(s):
         
     return set([frozenset(subset) for subset in sets] + [frozenset()])
 
-
+def powerset_alt(s):
+    """ Much slower way of getting the powerset."""
+    tuples = cartesian(*[s for _ in range(len(s))])
+    return set([frozenset(item) for item in tuples] + [frozenset()])
+    
+    
+    pass
 def breakRSA(e, n, message = None):
     """ Breaks a 'nice' RSA encryption with public key '(e, n)'."""
     (p, _), (q, _)  = primefact(n)
@@ -105,26 +122,21 @@ def choose(n, r):
     """ Combinations of 'r' subitems in a set of 'n' items"""
     return permute(n, r) / factorial(r)
 
-def pigeon(bins, num_to_fill):
-    """ Pigeon hole principle with 'n' boxes needing
-        'num_to_fill' items to fill them."""
-    return bins * (num_to_fill - 1) + 1
-
-def cardinality(s1, s2):
-    """ Cardinality of 's1' and 's2'"""
-    return len(set.union(s1,s2))
-
 permutations = permute
 combinations = choose
 
+
+def pigeon(bins, pigeons):
+    """ Pigeon hole principle with 'n' boxes needing
+        'pigeons' items to fill them.  Gives the 'smallest maximum'
+        that a bin/box must hold no matter how the 'pigeons' are arranged."""
+    return pigeons // bins + bool(pigeons % bins)
 
 def organize(seq):
     f = factorial(sum(seq))
     return f/(reduce(lambda x, y: x * y, [factorial(n) for n in seq]))
 
 if __name__ == "__main__":
-    pass
-
 
 
 
